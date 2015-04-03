044c5d3ed07b48e0a7fb49e75b389a402f021598
pierrelevy <pierre@androidsoft.org>
2015-04-03 13:57:35 +0200

    SQLPAGE-1 : Initial work - SQL script fix

--- src/sql/plugins/sqlpage/core/init_core_sqlpage.sql
+++ src/sql/plugins/sqlpage/core/init_core_sqlpage.sql
@@ -2,9 +2,9 @@
 --
 -- Data for table core_admin_right
 --
-DELETE FROM core_admin_right WHERE id_right = 'UPDATER_MANAGEMENT';
+DELETE FROM core_admin_right WHERE id_right = 'SQLPAGE_MANAGEMENT';
 INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is_updatable,plugin_name,id_feature_group,icon_url,documentation_url, id_order ) VALUES 
-('SQLPAGE_MANAGEMENT','sqlpage.adminFeature.ManageSQLPage.name',1,'jsp/admin/plugins/sqlpage/ManageSQLFragments.jsp','sqlpage.adminFeature.ManageSQLPage.description',0,'sqlpage',NULL,NULL,NULL,4);
+('SQLPAGE_MANAGEMENT','sqlpage.adminFeature.ManageSQLPage.name',1,'jsp/admin/plugins/sqlpage/ManageSQLPages.jsp','sqlpage.adminFeature.ManageSQLPage.description',0,'sqlpage',NULL,NULL,NULL,4);
 
 
 --
